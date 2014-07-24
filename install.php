76d94bda937c5511fb18c583bac4840d72f0d3aa
Goncalo Silva Dias <goncalo.dias@uberdigital.com>
2014-07-24 17:58:03 +0100

    added condition to create users table if not exists

--- install.php
+++ install.php
@@ -5,12 +5,20 @@ include_once('config' . PHPEXT);
 
 $mysql->statement('SHOW TABLES;');
 
+$table['users'] = 1;
+
 if ($mysql->total) {
 
     foreach($mysql->result() as $table) {
         print_r($table[0]);
+        if ($table[0] == 'users') {
+            $table['users'] = 0;
+        }
         flush();
     }
 }
 
+if ($table['users']) {
+    $mysql->statement(file_get_contents('sql/users.sql'));
+}
 $tpl->sendout();
