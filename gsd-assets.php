4492e7e280d11ea21c75277fa679a1a436b573c6
Goncalo Silva Dias <goncalo.dias@uberdigital.com>
2014-08-06 18:00:20 +0100

    detects asset image

--- gsd-assets.php
+++ gsd-assets.php
@@ -1,18 +1,20 @@
 <?php
 
-$iid = explode('_', $site->path[1]);
+if (@$site->path[1] == 'images') {
+    $iid = explode('_', @$site->path[2]);
 
-$mysql->statement('SELECT extension FROM images WHERE iid = :iid;', array(':iid' => $iid[0]));
+    $mysql->statement('SELECT extension FROM images WHERE iid = :iid;', array(':iid' => $iid[0]));
 
-$asset = sprintf('gsd-assets/images/%d/%d.%s', $iid[0], $iid[0], $mysql->singleresult());
+    $asset = sprintf('gsd-assets/images/%d/%d.%s', $iid[0], $iid[0], $mysql->singleresult());
 
-if ($mysql->total) {
-    $size = getimagesize($asset);
+    if ($mysql->total) {
+        $size = getimagesize($asset);
 
-    $fp = fopen($asset, "rb");
-    if ($size && $fp) {
-        header("Content-type: {$size['mime']}");
-        fpassthru($fp);
-        exit;
+        $fp = fopen($asset, "rb");
+        if ($size && $fp) {
+            header("Content-type: {$size['mime']}");
+            fpassthru($fp);
+            exit;
+        }
     }
 }
