779e4baea9f67216b867eeb702d97fcbc190c389
Goncalo Silva Dias <goncalo.dias@uberdigital.com>
2014-08-07 12:53:54 +0100

    fix redirect bug

--- gsd-admin/pages/actions/settings.php
+++ gsd-admin/pages/actions/settings.php
@@ -2,22 +2,24 @@
 
 if (@$_REQUEST['save']) {
 
-    $mysql->statement('SELECT url FROM pages WHERE pid = ?;', array($path[2]));
+    $mysql->statement('SELECT url FROM pages WHERE pid = ?;', array(@$site->path[2]));
     
     $currenturl = $mysql->singleresult();
+    
+    $mysql->statement('DELETE FROM redirect WHERE `from` = ?;', array($_REQUEST['url']));
     
     $mysql->statement('SELECT `from`, destination FROM redirect WHERE destination = ? ORDER BY created;', array($currenturl));
     
     if ($mysql->total) {
         foreach($mysql->result() as $url) {
-            $mysql->statement('INSERT INTO redirect (`pid`, `from`, `destination`, `creator`) VALUES (?, ?, ?, ?);', array($path[2], $url[1], $_REQUEST['url'], $user->id));
+            $mysql->statement('INSERT INTO redirect (`pid`, `from`, `destination`, `creator`) VALUES (?, ?, ?, ?);', array(@$site->path[2], $url[1], $_REQUEST['url'], $user->id));
         }
     } else {
-        $mysql->statement('INSERT INTO redirect (`pid`, `from`, `destination`, `creator`) VALUES (?, ?, ?, ?);', array($path[2], $currenturl, $_REQUEST['url'], $user->id));
+        $mysql->statement('INSERT INTO redirect (`pid`, `from`, `destination`, `creator`) VALUES (?, ?, ?, ?);', array(@$site->path[2], $currenturl, $_REQUEST['url'], $user->id));
     }
     $mysql->statement('UPDATE pages SET url = ? WHERE pid = ?;', array(
         $_REQUEST['url'],
-        $path[2]
+        @$site->path[2]
     ));
     
     header("Location: /admin/pages", true, 302);
