7169f11241700380a76a9bc3795239ad7136d34b
Goncalo Silva Dias <goncalo.dias@uberdigital.com>
2014-08-07 14:31:47 +0100

    adding random password to create user

--- gsd-admin/users/actions/create.php
+++ gsd-admin/users/actions/create.php
@@ -5,18 +5,21 @@ if (@$_REQUEST['save']) {
     include_once(CLIENTPATH . 'include/admin/fields' . PHPEXT);
     
     $extrafields = function_exists('usersfields') ? usersfields() : array();
+    
+    $password = substr(str_shuffle(sha1(rand() . time() . "gsdias-cms")), 2, 10);
 
-    $fields = array('email', 'level', 'name', 'creator');
+    $fields = array('email', 'password', 'level', 'name', 'creator');
     
     $values = array(
         $_REQUEST['email'],
+        md5($password),
         $_REQUEST['level'],
         $_REQUEST['name'],
         $user->id
     );    
     
-    foreach ($sectionextrafields['list'] as $key => $field) {
-        $fields[] = $sectionextrafields['list'][$key];
+    foreach ($extrafields['list'] as $key => $field) {
+        $fields[] = $extrafields['list'][$key];
         $values[] = @$_REQUEST[$field];
     }
         
@@ -25,6 +28,15 @@ if (@$_REQUEST['save']) {
     $mysql->statement(sprintf('INSERT INTO users (%s) values(%s);', implode(', ', $fields), substr($questions, 2)), $values);
     
     if ($mysql->total) {
+        $email = new email();
+        
+        $email->setto($_REQUEST['email']);
+        $email->setfrom($site->email);
+        $email->setreplyto($site->email);
+        $email->setsubject('Registo no site');
+        $email->setbody(sprintf('Foi criado um registo com este email. Para poder aceder use o seu email e a password: ', $password));
+        
+        $email->sendmail();        
         header("Location: /admin/users", true, 302);
     }
 }
