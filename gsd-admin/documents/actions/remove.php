c8e1b9c7e986ab27b09f612c3cad70f4a2deb5eb
Goncalo Silva Dias <goncalo.dias@uberdigital.com>
2014-08-14 18:02:38 +0100

    added remove confirm layer

--- gsd-admin/documents/actions/remove.php
+++ gsd-admin/documents/actions/remove.php
@@ -1,10 +1,17 @@
 <?php
 
-removefile(ASSETPATH . 'documents/' . $site->arg(2));
+if (@$_REQUEST['confirm'] == 'Sim') {
+    removefile(ASSETPATH . 'documents/' . $site->arg(2));
 
-$mysql->statement('DELETE FROM documents WHERE did = ?;', array($site->arg(2)));
+    $mysql->statement('DELETE FROM documents WHERE did = ?;', array($site->arg(2)));
 
-if ($mysql->total) {
+    if ($mysql->total) {
+        header("Location: /admin/documents", true, 302);
+        exit;
+    }
+}
+
+if (@$_REQUEST['confirm'] == 'Nao') {
     header("Location: /admin/documents", true, 302);
     exit;
 }
