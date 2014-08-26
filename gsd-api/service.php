fed73110ecd4c2f74100b0c5e2adce3913fa6996
Gonçalo Silva Dias <mail@gsdias.pt>
2014-08-26 18:17:16 +0100

    api

--- gsd-api/service.php
+++ gsd-api/service.php
@@ -3,14 +3,14 @@
 require_once('_other' . PHPEXT);
 require_once('_' . strtolower($_SERVER['REQUEST_METHOD']) . PHPEXT);
 
-$other = '../resources/api/_other' . PHPEXT;
+$other = CLIENTPATH . 'api/_other' . PHPEXT;
 
-if (file_exists($other)) {
+if (is_file($other)) {
     require_once($other);
 }
 
-$method = '../resources/api/_' . strtolower($_SERVER['REQUEST_METHOD']) . PHPEXT;
+$method = CLIENTPATH . 'api/_' . strtolower($_SERVER['REQUEST_METHOD']) . PHPEXT;
 
-if (file_exists($method)) {
+if (is_file($method)) {
     require_once($method);
 }
