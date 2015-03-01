2f19ebca7c1c20f5cfb55b670fc36ba7333bc98d
Goncalo Silva Dias <goncalo.silvadias@akqa.com>
2015-03-01 18:24:48 +0000

    navigation

--- gsd-assets.php
+++ gsd-assets.php
@@ -4,11 +4,11 @@ if (@$site->arg(1) == 'images') {
     $iid = explode('_', @$site->arg(2));
     $name = explode('.', @$site->arg(2));
 
-    $mysql->statement('SELECT iid, extension FROM images WHERE iid = ? OR (name = ? AND extension = ?);', array($iid[0], $name[0], $name[1]));
+    $mysql->statement('SELECT iid, extension FROM images WHERE iid = ? OR (name = ? AND extension = ?);', array($iid[0], $name[0], @$name[1]));
 
     $image = $mysql->singleline();
 
-    $asset = sprintf('gsd-assets/images/%d.%s', $image['iid'], $image['extension']);
+    $asset = sprintf('gsd-assets/images/%d.%s', @$image['iid'], @$image['extension']);
 
     if ($mysql->total) {
         $size = getimagesize($asset);
