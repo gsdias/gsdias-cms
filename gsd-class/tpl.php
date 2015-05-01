d4775a9b91ae3f4aca0f74aba4e492174b8733f9
Goncalo Silva Dias <goncalo.silvadias@akqa.com>
2015-05-01 17:20:52 +0100

    version 1.2

--- gsd-class/tpl.php
+++ gsd-class/tpl.php
@@ -3,7 +3,7 @@
 /**
  * @author     Goncalo Silva Dias <mail@gsdias.pt>
  * @copyright  2014-2015 GSDias
- * @version    1.1
+ * @version    1.2
  * @link       https://bitbucket.org/gsdias/gsdias-cms/downloads
  * @since      File available since Release 1.0
  */
--- gsd-development/sass/modules/_submodule.scss
+++ gsd-development/sass/modules/_submodule.scss
@@ -1,7 +1,7 @@
 /**
  * @author     Goncalo Silva Dias <mail@gsdias.pt>
  * @copyright  2014-2015 GSDias
- * @version    1.1
+ * @version    1.2
  * @link       https://bitbucket.org/gsdias/gsdias-cms/downloads
  * @since      File available since Release 1.0
  */
