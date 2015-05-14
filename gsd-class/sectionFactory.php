70a286ef2191617d4ff7008a2fdf77e8e5f8cec0
Goncalo Silva Dias <goncalo.silvadias@akqa.com>
2015-05-14 22:49:07 +0100

    v1.3

--- gsd-class/sectionFactory.php
+++ gsd-class/sectionFactory.php
@@ -4,7 +4,7 @@
  * @author     Goncalo Silva Dias <mail@gsdias.pt>
  * @copyright  2014-2015 GSDias
  *
- * @version    1.2
+ * @version    1.3
  *
  * @link       https://bitbucket.org/gsdias/gsdias-cms/downloads
  * @since      File available since Release 1.2
