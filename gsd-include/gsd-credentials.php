6e68efd7d57016253c264c6337fe9b1d2158e137
Gonçalo Silva Dias <goncalo.dias@landmarkwebteam.com>
2016-04-17 17:04:25 +0400

    v1.5

--- gsd-include/gsd-credentials.php
+++ gsd-include/gsd-credentials.php
@@ -4,7 +4,7 @@
  * @author     Goncalo Silva Dias <mail@gsdias.pt>
  * @copyright  2014-2015 GSDias
  *
- * @version    1.4
+ * @version    1.5
  *
  * @link       https://bitbucket.org/gsdias/gsdias-cms/downloads
  * @since      File available since Release 1.0
